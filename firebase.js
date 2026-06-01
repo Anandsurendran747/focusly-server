1d25defc49fbf6e1977a03a4e1b8e4eb86ec1d87
Anand S <anandsurendran747@gmail.com>
2026-06-01 18:19:59 +0530

    changed checker

--- firebase.js
+++ firebase.js
@@ -1,23 +1,13 @@
 const admin = require("firebase-admin");
 const dotenv = require("dotenv");
-
 dotenv.config();
-console.log("projectId:", process.env.FIREBASE_PROJECT_ID);
-console.log("clientEmail:", process.env.FIREBASE_CLIENT_EMAIL);
-console.log("privateKey first 100:", process.env.FIREBASE_PRIVATE_KEY?.slice(0, 100));
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY
-    .replace(/\\n/g, '\n')
-    .replace(/"/g, '');
-
-admin.initializeApp({
-    credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,    // ✅ camelCase
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: privateKey
-    })
-});
-
-console.log("✅ Firebase Admin initialized");
+if (!admin.apps.length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+    console.log("✅ Firebase Admin initialized");
+}
 
 module.exports = admin;
